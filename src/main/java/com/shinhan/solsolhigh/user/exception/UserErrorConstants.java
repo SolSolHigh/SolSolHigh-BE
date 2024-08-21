e0f532c6d7b491c1a61662bfb2796f79a240aec9
yuseung Lee <yuseung0429@naver.com>
2024-08-21 21:16:20 +0900

    fix/#15: SIGNUP_NOT_COMPLETED 응답 코드 변경

--- src/main/java/com/shinhan/solsolhigh/user/exception/UserErrorConstants.java
+++ src/main/java/com/shinhan/solsolhigh/user/exception/UserErrorConstants.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorConstants implements ErrorConstantDefinition {
-    SIGNUP_NOT_COMPLETED("회원가입 미완료", "A001", HttpStatus.NOT_FOUND);
+    SIGNUP_NOT_COMPLETED("회원가입 미완료", "U004", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final String statusCode;
